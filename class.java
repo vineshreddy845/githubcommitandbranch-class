63570061badb12f853a5cac4695d1c479cbb8d93
vineshreddy845 <64718480+vineshreddy845@users.noreply.github.com>
2022-06-27 13:13:23 -0500

    Create class.java

---
+++ class.java
@@ -0,0 +1,13 @@
+class Class
+{
+  public void str()
+  {
+    System.out.println("hello");
+   }
+   public static void main(String args[])
+   {
+     Class obj=new Class();
+     obj.str();
+   }
+ }
+ 
