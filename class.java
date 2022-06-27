7670454580d62894ed75aa368ce78a5e001c7b85
vineshreddy845 <64718480+vineshreddy845@users.noreply.github.com>
2022-06-27 13:46:04 -0500

    vinesh function is added

--- class.java
+++ class.java
@@ -7,9 +7,15 @@ class Class {
     System.out.println("hi simbha---");
   }
 
+  int vinesh(int x) {
+    return x;
+  }
+
   public static void main(String args[]) {
+    // int x;
     Class obj = new Class();
     obj.str();
     simbha();
+    System.out.println(obj.vinesh(2));
   }
 }
