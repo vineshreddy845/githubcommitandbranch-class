f5a8bab2f6ffb5b1f930f1dff36910550cac5124
vineshreddy845 <64718480+vineshreddy845@users.noreply.github.com>
2022-06-27 13:53:58 -0500

    function vineeth is added

--- class.java
+++ class.java
@@ -6,10 +6,14 @@ class Class {
   static void simbha() {
     System.out.println("hi simbha---");
   }
-
+  int vineeth(int y)
+  {
+    return y;
+  } 
   public static void main(String args[]) {
     Class obj = new Class();
     obj.str();
     simbha();
+    System.out.println(obj.vineeth(2));
   }
 }
